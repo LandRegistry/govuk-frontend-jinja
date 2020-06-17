1cf2d8ee8e073ed827df2dcc05ed2a0a0f559ad9
Andy Mantell <andy.mantell@surevine.com>
2020-06-17 09:19:48 +0100

    Update to latest version of govuk-frontend-diff

--- tests/utils/render.py
+++ tests/utils/render.py
@@ -13,16 +13,17 @@ env = Environment(loader=loader, autoescape=True)
 
 parser = argparse.ArgumentParser(description='Render a govuk_frontend_jinja template.')
 parser.add_argument('--component', metavar='Component name', nargs=1, help='The name of the component')
-parser.add_argument('--template', dest='template', action='store_true', help='Flag to determine that we are rendering the base template')
+parser.add_argument('--template', dest='template', action='store_true',
+                    help='Flag to determine that we are rendering the base template')
 parser.set_defaults(template=True)
 parser.add_argument('--params', metavar='Params', nargs=1,
                     help='JSON representation of the params to pass to the component macro')
 
 args = parser.parse_args()
 
+
 def hyphenated_to_camel(string):
     return ''.join(x.capitalize() for x in string.split('-'))
-
 
 
 if args.component:
@@ -30,12 +31,12 @@ if args.component:
         {{% from "govuk_frontend_jinja/components/" + component + "/macro.html" import govuk{macro_name} %}}
         {{{{ govuk{macro_name}(params) }}}}
     '''.format(macro_name=hyphenated_to_camel(args.component[0])))
-    
+
     print(template.render(
         component=args.component[0],
         params=json.loads(args.params[0])
     ))
-   
+
 
 elif args.template:
 
@@ -48,7 +49,7 @@ elif args.template:
         {% block skipLink %}{% if skipLink %}{{ skipLink }}{% else %}{{ super() }}{% endif %}{% endblock %}
         {% block header %}{% if header %}{{ header }}{% else %}{{ super() }}{% endif %}{% endblock %}
         {% block main %}{% if main %}{{ main }}{% else %}{{ super() }}{% endif %}{% endblock %}
-        {% block beforeContent %}{% if beforeContent %}{{ beforeContent }}{% else %}{{ super() }}{% endif %}{% endblock %}
+        {% block beforeContent %}{% if beforeContent %}{{ beforeContent }}{% else %}{{ super() }}{% endif %}{% endblock %} # noqa: E501
         {% block content %}{% if content %}{{ content }}{% else %}{{ super() }}{% endif %}{% endblock %}
         {% block footer %}{% if footer %}{{ footer }}{% else %}{{ super() }}{% endif %}{% endblock %}
         {% block bodyEnd %}{% if bodyEnd %}{{ bodyEnd }}{% else %}{{ super() }}{% endif %}{% endblock %}
@@ -56,5 +57,3 @@ elif args.template:
 
     context = json.loads(args.params[0])
     print(template.render(**context))
-
-
