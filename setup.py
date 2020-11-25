ef9b8e3dfa8ad0529944c0c41e96511ade4fe21c
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-11-25 23:47:28 +0000

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for directory in directories:
 
 setuptools.setup(
     name="govuk-frontend-jinja",
-    version="1.0.0",
+    version="1.1.0",
     author="Matt Shaw",
     author_email="matthew.shaw@landregistry.gov.uk",
     description="GOV.UK Frontend Jinja Macros",
