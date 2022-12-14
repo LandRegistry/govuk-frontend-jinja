5b59e489bbcec6b21d5eea3932e6cb0d4472a09c
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-12-14 16:16:23 +0000

    add supported python versions to classifiers

--- setup.py
+++ setup.py
@@ -24,7 +24,11 @@ setuptools.setup(
     package_data={"govuk_frontend_jinja": components},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Environment :: Web Environment",
         "Operating System :: OS Independent",
