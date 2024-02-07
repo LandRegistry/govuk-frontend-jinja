53754b90e6f71d9737412237f53fbc856bbc139d
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-02-07 09:20:15 +0000

    prepare v1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for directory in directories:
 
 setuptools.setup(
     name="govuk-frontend-jinja",
-    version="1.5.1",
+    version="1.6.0",
     author="Matt Shaw",
     author_email="matthew.shaw@landregistry.gov.uk",
     description="GOV.UK Frontend Jinja Macros",
